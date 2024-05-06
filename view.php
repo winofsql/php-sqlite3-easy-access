05779510049a63839f851490643dc9d923a2cdc5
sworc <lightbox@nifty.com>
2024-05-06 15:21:37 +0900

    Add files via upload

---
+++ view.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>PHP Test</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<h1 class="alert alert-primary">社員一覧</h1>
+
+<div class="pt-2 ps-5 pb-2">
+<?= $result ?>
+</div>
+
+<h1 class="alert alert-danger">フッター</h1>
+</body>
+</html>
